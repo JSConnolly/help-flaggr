9b9f12d36272ad1c5c24e5742f9ed2247dbdc242
J Connolly <jsconnolly1@gmail.com>
2013-04-05 11:48:44 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelpFlaggr::Application.config.secret_token = 'bfbf2fbf0061c71abed134a711a54f0dc71345a6d1c7cc9c1f249fd2f85225e8924d53b7e32456e5009b1d8a7a2663c2392dd26848ff42413a857ce03d420b37'
