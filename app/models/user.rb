97856b20f8e3ebed9e8a81dcfc963fdec589e8de
Julian <juliantai90@gmail.com>
2013-04-06 17:53:21 -0700

    phase update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :phase
   validates :email, presence: true, uniqueness: true, format: {with: /^\w+@\w+\.[a-zA-Z]{2,}/}
-  validates :phase, presence: true, inclusion: { in: [-1, 0, 1, 2, 3, 4] }
+  validates :phase, presence: true, inclusion: { in: [1, 2, 3, 4, 5, 6] }
 
   has_and_belongs_to_many :help_requests 
   belongs_to :help_request
@@ -16,5 +16,5 @@ class User < ActiveRecord::Base
     # c.integer :user_id
 
 
-  
+
 end
